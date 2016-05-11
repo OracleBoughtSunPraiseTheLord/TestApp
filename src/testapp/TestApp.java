e325d3766b06f0e03458871972926cd7d6346096
martin.bartos <martin.bartos@B110-09.pslib.cz>
2016-05-11 10:06:28 +0200

    treti

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -2,11 +2,9 @@ package testapp;
 public class TestApp {
     public static void main(String[] args) {
 
-        System.out.println("Hell o world");
-        System.out.println("How are you?");
-        System.out.println("Is this a real life, is this just fanta sea?");
-        System.out.println("No escape from reality... Open your eyes, up to the sky and SEEEEEEEE");
-        System.out.println("IM JUST A POOR BOY, I NEED NO SYMPATHY");
+       System.out.println("Ahoje");
+       System.out.println("Jak se vede?");
+         
         
         
         
